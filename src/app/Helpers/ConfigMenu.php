0612f6cbda17d0c0ca118365969f5d7355e36f5d
duylive <duylive92@gmail.com>
2020-11-11 12:01:02 +0700

    fix ConfigMenu & route api README

--- src/app/Helpers/ConfigMenu.php
+++ src/app/Helpers/ConfigMenu.php
@@ -4,17 +4,25 @@ namespace VCComponent\Laravel\Dynamic\Helpers;
 
 class ConfigMenu
 {
+    const SYSTEM_USER = "system_user";
+    const SYSTEM_SCRIPT = "system_script";
+    const SYSTEM_ROLE = "system_role";
+    const SYSTEM_CONFIGURATION = "system_configuration";
+    const SYSTEM_CONFIG_MAIL = "system_config_mail";
+    const SYSTEM_REDIRECTOR = "system_redirector";
+    const SYSTEM_SITEMAP = "system_sitemap";
     const USER = "user";
-    const SCRIPT_MANAGER = "script_manager";
     const CONTACT = "contact";
     const LANGUAGE = "language";
-    const FOLLOW = "follow";
+    const SUBSCRIBE = "subscribe";
     const MEDIA = "media";
     const POST = "post";
-    const ORTHER_POST = "orther_post";
+    const ORTHER_POST = "other_post";
     const PAGE = "page";
-    const PRODUCTS = "products";
+    const MENU = "menu";
+    const PRODUCT = "product";
     const ORDER = "order";
     const COMMENT = "comment";
     const TAG = "tag";
+    const FOLLOW = "follow";
 }
